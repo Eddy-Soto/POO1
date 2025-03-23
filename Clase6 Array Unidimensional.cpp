e260022fad9193cbc661cc53554d8845a7bd8b20
Eddy-Soto <esotoh@miumg.edu.gt>
2025-03-22 22:54:17 -0600

    Add files via upload

---
+++ Clase6 Array Unidimensional.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+main(){
+	
+    string semana [7]={"lunes","martes","miercoles","jueves","viernes","sabado","domingo"};
+    
+        //semana[3] = "jueves"
+        for (int i=0;i<7;i++){
+        	cout<<semana[i]<<endl;
+		}
+	
+	system ("pause");
+}
+
